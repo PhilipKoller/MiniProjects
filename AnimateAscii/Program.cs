63a7f9cf01140383162f888cf33662ad4091ef46
PhilipKoller <philip.koller@outlook.com>
2021-02-24 22:58:10 -0600

    Added second animation and async user input

--- AnimateAscii/Program.cs
+++ AnimateAscii/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AnimateAscii
 {
@@ -9,10 +10,10 @@ namespace AnimateAscii
         {
             Animate.LoadAscii();
             SetConsole();
+            GetUserInput();
 
             while (true)
             {
-
                 Thread.Sleep(50);
                 Animate.UpdateProgress();
             }
@@ -26,7 +27,22 @@ namespace AnimateAscii
 
             Console.SetBufferSize(220, 242);
         }
-    }
+        public static async void GetUserInput()
+        {
+            // Allows user to type whithout freexing the UI 
+            var t = Task.Run(async delegate
+            {
+                int userNumber = Int32.Parse(Console.ReadLine());
+                if (userNumber == 1)
+                {
+                    //CHANGES AND RESETS ANIMATION FRAME COUNTER
+                    Animate._currentFrame = 0;
+                    Animate._animationNum = 1;
+                }
+            });
+        }
+        
+        }
 
     
 }
